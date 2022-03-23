db4c2c9cee22e678563a0830f3c5d66a345395f4
Yuborama <davidjohan2@hotmail.com>
2022-03-23 02:07:53 -0500

    fead: add conditon tlaxcala

--- src/assets/dataMap.ts
+++ src/assets/dataMap.ts
@@ -1,369 +1,369 @@
-const colorFill = '#00000029';
-const colorBorder = '#00000020';
+const colorFill = "#00000029";
+const colorBorder = "#00000020";
 
 export const data = [
   {
-    id: 'Baja California',
+    id: "Baja California",
     fill: colorBorder,
     stroke: colorBorder,
 
     disable: false,
-    data: '<div><h2 style={{ color: `#fff`, fontSize: `20px` }}>Baja California</h2><p>Prueba de contenido</p></div>'
+    data: "<div><h2 style={{ color: `#fff`, fontSize: `20px` }}>Baja California</h2><p>Prueba de contenido</p></div>",
   },
   {
-    id: 'Baja California Sur',
+    id: "Baja California Sur",
     fill: colorFill,
     stroke: colorBorder,
     disable: true,
-    data: '<div><h2 style={{ color: `#fff`, fontSize: `20px` }}>Baja California Sur</h2><p>Prueba de contenido</p></div>'
+    data: "<div><h2 style={{ color: `#fff`, fontSize: `20px` }}>Baja California Sur</h2><p>Prueba de contenido</p></div>",
   },
   {
-    id: 'Coahuila',
+    id: "Coahuila",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<div><h2>Coahuila</h2><p>Prueba de contenido</p></div>'
+    data: "<div><h2>Coahuila</h2><p>Prueba de contenido</p></div>",
   },
   {
-    id: 'Chihuahua',
+    id: "Chihuahua",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Durango',
+    id: "Durango",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Sinaloa',
+    id: "Sinaloa",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Sonora',
+    id: "Sonora",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Zacatecas',
+    id: "Zacatecas",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Nuevo León',
+    id: "Nuevo León",
     fill: colorFill,
     stroke: colorBorder,
     disable: true,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'San Luis Potosí',
+    id: "San Luis Potosí",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Tamaulipas',
+    id: "Tamaulipas",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Aguascalientes',
+    id: "Aguascalientes",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Colima',
+    id: "Colima",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Jalisco',
+    id: "Jalisco",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Michoacán',
+    id: "Michoacán",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Nayarit',
+    id: "Nayarit",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Campeche',
+    id: "Campeche",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Oaxaca',
+    id: "Oaxaca",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido 333</p>'
+    data: "<p>Prueba de contenido 333</p>",
   },
   {
-    id: 'Puebla',
+    id: "Puebla",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Tabasco',
+    id: "Tabasco",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Tlaxcala',
+    id: "Tlaxcala",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'CDMX',
+    id: "CDMX",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Guanajuato',
+    id: "Guanajuato",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Guerrero',
+    id: "Guerrero",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Hidalgo',
+    id: "Hidalgo",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Estado de México',
+    id: "Estado de México",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Morelos',
+    id: "Morelos",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Querétaro',
+    id: "Querétaro",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Veracruz',
+    id: "Veracruz",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Chiapas',
+    id: "Chiapas",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Quintana Roo',
+    id: "Quintana Roo",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
+    data: "<p>Prueba de contenido</p>",
   },
   {
-    id: 'Yucatán',
+    id: "Yucatán",
     fill: colorFill,
     stroke: colorBorder,
     disable: false,
-    data: '<p>Prueba de contenido</p>'
-  }
+    data: "<p>Prueba de contenido</p>",
+  },
 ];
 
 const statesActives = [
-  'Baja California',
-  'Chiapas',
-  'Chihuahua',
-  'Estado de México',
-  'Coahuila',
-  'Guanajuato',
-  'Jalisco',
-  'Sonora',
-  'Sinaloa',
-  'Quintana Roo',
-  'Veracruz',
-  'Querétaro',
-  'Oaxaca',
-  'Puebla',
-  'Tlaxcala',
-  'Nuevo León',
-  'Michoacán',
-  'CDMX'
+  "Baja California",
+  "Chiapas",
+  "Chihuahua",
+  "Estado de México",
+  "Coahuila",
+  "Guanajuato",
+  "Jalisco",
+  "Sonora",
+  "Sinaloa",
+  "Quintana Roo",
+  "Veracruz",
+  "Querétaro",
+  "Oaxaca",
+  "Puebla",
+  "Tlaxcala",
+  "Nuevo León",
+  "Michoacán",
+  "CDMX",
 ];
 const states = [
   {
-    state: 'Baja California',
+    state: "Baja California",
     ifqs: 4,
-    invertion: '$1,741,692',
-    beneficiaries: '1,784'
+    invertion: "$1,741,692",
+    beneficiaries: "1,784",
   },
   {
-    state: 'Chiapas',
+    state: "Chiapas",
     ifqs: 1,
-    invertion: '$602,650',
-    beneficiaries: '175'
+    invertion: "$602,650",
+    beneficiaries: "175",
   },
   {
-    state: 'Chihuahua',
+    state: "Chihuahua",
     ifqs: 2,
-    invertion: '$736,416',
-    beneficiaries: '1,127'
+    invertion: "$736,416",
+    beneficiaries: "1,127",
   },
   {
-    state: 'Ciudad de México',
+    state: "Ciudad de México",
     ifqs: 20,
-    invertion: '$11,816,932',
-    beneficiaries: '5,603'
+    invertion: "$11,816,932",
+    beneficiaries: "5,603",
   },
   {
-    state: 'Coahuila',
+    state: "Coahuila",
     ifqs: 2,
-    invertion: '$940,971',
-    beneficiaries: '177'
+    invertion: "$940,971",
+    beneficiaries: "177",
   },
   {
-    state: 'Guanajuato',
+    state: "Guanajuato",
     ifqs: 1,
-    invertion: '$745,170',
-    beneficiaries: '119'
+    invertion: "$745,170",
+    beneficiaries: "119",
   },
   {
-    state: 'Jalisco',
+    state: "Jalisco",
     ifqs: 6,
-    invertion: '$3,782,640',
-    beneficiaries: '1,127'
+    invertion: "$3,782,640",
+    beneficiaries: "1,127",
   },
   {
-    state: 'Estado de México',
+    state: "Estado de México",
     ifqs: 1,
-    invertion: '$142,393',
-    beneficiaries: '35'
+    invertion: "$142,393",
+    beneficiaries: "35",
   },
   {
-    state: 'Michoacán',
+    state: "Michoacán",
     ifqs: 1,
-    invertion: '$563,918',
-    beneficiaries: '269'
+    invertion: "$563,918",
+    beneficiaries: "269",
   },
   {
-    state: 'Nuevo León',
+    state: "Nuevo León",
     ifqs: 4,
-    invertion: '$1,973,409',
-    beneficiaries: '2,755'
+    invertion: "$1,973,409",
+    beneficiaries: "2,755",
   },
   {
-    state: 'Oaxaca',
+    state: "Oaxaca",
     ifqs: 2,
-    invertion: '$858,920',
-    beneficiaries: '195'
+    invertion: "$858,920",
+    beneficiaries: "195",
   },
   {
-    state: 'Puebla',
+    state: "Puebla",
     ifqs: 2,
-    invertion: '$1,440,227',
-    beneficiaries: '245'
+    invertion: "$1,440,227",
+    beneficiaries: "245",
   },
   {
-    state: 'Querétaro',
+    state: "Querétaro",
     ifqs: 2,
-    invertion: '$1,300,625',
-    beneficiaries: '194'
+    invertion: "$1,300,625",
+    beneficiaries: "194",
   },
   {
-    state: 'Quintana Roo',
+    state: "Quintana Roo",
     ifqs: 1,
-    invertion: '$556,733',
-    beneficiaries: '2,607'
+    invertion: "$556,733",
+    beneficiaries: "2,607",
   },
   {
-    state: 'Sinaloa',
+    state: "Sinaloa",
     ifqs: 1,
-    invertion: '$449,301',
-    beneficiaries: '2,335'
+    invertion: "$449,301",
+    beneficiaries: "2,335",
   },
   {
-    state: 'Sonora',
+    state: "Sonora",
     ifqs: 2,
-    invertion: '$999,995',
-    beneficiaries: '2,037'
+    invertion: "$999,995",
+    beneficiaries: "2,037",
   },
   {
-    state: 'Veracruz',
+    state: "Veracruz",
     ifqs: 1,
-    invertion: '$501,402',
-    beneficiaries: '46'
+    invertion: "$501,402",
+    beneficiaries: "46",
   },
   {
-    state: 'CDMX',
+    state: "CDMX",
     ifqs: 20,
-    invertion: '$11,816,932',
-    beneficiaries: '5,603'
+    invertion: "$11,816,932",
+    beneficiaries: "5,603",
   },
   {
-    state: 'Tlaxcala',
+    state: "Tlaxcala",
     ifqs: 1,
-    invertion: '$1,854,852',
-    beneficiaries: '2,827'
-  }
+    invertion: "$1,854,852",
+    beneficiaries: "2,827",
+  },
 ];
 
 export const filterActive = (screenWidth: number) =>
@@ -372,24 +372,33 @@ export const filterActive = (screenWidth: number) =>
       ? {
           ...state,
           disable: screenWidth > 768 ? false : true,
-          stroke: screenWidth > 768 ? colorBorder : '#F8F8F8',
-          fill: screenWidth > 768 ? 'white' : '#F8F8F8',
-          data: `
+          stroke: screenWidth > 768 ? colorBorder : "#F8F8F8",
+          fill: screenWidth > 768 ? "white" : "#F8F8F8",
+          data:
+            states.find((item) => item.state === state.id)?.state === "Tlaxcala"
+              ? `
+              <div>
+              <p ><i>${
+                states.find((item) => item.state === state.id)?.state
+              }</i></p>  
+            </div>
+          `
+              : `
           <div>
             <p ><i>${
               states.find((item) => item.state === state.id)?.state
             }</i> ${states.find((item) => item.state === state.id)?.ifqs} / ${
-            states.find((item) => item.state === state.id)?.beneficiaries
-          }  </p>  
+                  states.find((item) => item.state === state.id)?.beneficiaries
+                }  </p>  
             <b>${states.find((item) => item.state === state.id)?.invertion} </b>
           </div>
-        `
+        `,
         }
       : {
           ...state,
           disable: true,
-          stroke: '#F8F8F8',
-          fill: screenWidth > 768 ? '#F8F8F8' : '#DE2D6E',
-          data: `<p>${state.id}</p>`
+          stroke: "#F8F8F8",
+          fill: screenWidth > 768 ? "#F8F8F8" : "#DE2D6E",
+          data: `<p>${state.id}</p>`,
         }
   );
